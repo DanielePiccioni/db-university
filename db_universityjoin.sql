52923c2ec97f6b0176ab29e388dbf6ed193f4f91
DanielePiccioni <danielepiccioni1@gmail.com>
2025-12-05 14:57:35 +0100

    query with join 2

--- db_universityjoin.sql
+++ db_universityjoin.sql
@@ -1,7 +1,16 @@
 #1. Selezionare tutti gli studenti iscritti al Corso di Laurea in Economia
 #students, degrees
 
-SELECT students.id, students.name, students.surname, degrees.name AS degree_name
+/*SELECT students.id, students.name, students.surname, degrees.name AS degree_name
 FROM students
 JOIN degrees ON students.degree_id = degrees.id
-WHERE degrees.name = 'Corso di Laurea in Economia';
+WHERE degrees.name = 'Corso di Laurea in Economia';*/
+
+#2. Selezionare tutti i Corsi di Laurea Magistrale del Dipartimento di Neuroscienze
+# degrees, departments
+
+SELECT degrees.id, degrees.name, degrees.level, departments.name AS department_name
+FROM degrees
+JOIN departments ON degrees.department_id = departments.id
+WHERE departments.name = "Dipartimento di Neuroscienze"
+AND degrees.level = "Magistrale"
